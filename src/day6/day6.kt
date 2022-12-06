84255950157ba303fbb443da1d0cd5901c0a9853
RickKelder <rickkelder@creetion.com>
2022-12-06 11:54:43 +0100

    day 6 ez mode

--- src/day6/day6.kt
+++ src/day6/day6.kt
@@ -10,14 +10,15 @@ fun main(args: Array<String>) {
     part2(lineList)
 }
 
+fun getUniqueSequence(line: String, seqSize: Int): Int {
+    line.toCharArray().asIterable().windowed(seqSize, 1, false).map { it.distinct() }.forEachIndexed { index, chars -> if (chars.size >= seqSize) return (index+seqSize) }
+    return -1
+}
+
 fun part1(lineList: List<String>) {
-    var indexFour = -1;
-    lineList[0].toCharArray().asIterable().windowed(4, 1, false).map { it.distinct() }.forEachIndexed { index, chars -> if (chars.size >= 4 && indexFour == -1) indexFour = index+4 }
-    println("part1: $indexFour")
+    println("part1: "+ getUniqueSequence(lineList[0], 4))
 }
 
 fun part2(lineList: List<String>) {
-    var indexFourteen = -1;
-    lineList[0].toCharArray().asIterable().windowed(14, 1, false).map { it.distinct() }.forEachIndexed { index, chars -> if (chars.size >= 14 && indexFourteen == -1) indexFourteen = index+14 }
-    println("part2: $indexFourteen")
+    println("part1: "+ getUniqueSequence(lineList[0], 14))
 }
