65bff7257941f3afc1ef932f8d4d265844222833
RickKelder <rickkelder@creetion.com>
2022-12-06 11:45:52 +0100

    day 6 ez mode

--- src/day6/day6.kt
+++ src/day6/day6.kt
@@ -12,12 +12,12 @@ fun main(args: Array<String>) {
 
 fun part1(lineList: List<String>) {
     var indexFour = -1;
-    lineList[0].toCharArray().asIterable().windowed(4, 1, false).map { it.distinct() }.forEachIndexed { index, chars -> if (chars.size > 3 && indexFour == -1) indexFour = index+4 }
+    lineList[0].toCharArray().asIterable().windowed(4, 1, false).map { it.distinct() }.forEachIndexed { index, chars -> if (chars.size >= 4 && indexFour == -1) indexFour = index+4 }
     println("part1: $indexFour")
 }
 
 fun part2(lineList: List<String>) {
     var indexFourteen = -1;
-    lineList[0].toCharArray().asIterable().windowed(14, 1, false).map { it.distinct() }.forEachIndexed { index, chars -> if (chars.size > 13 && indexFourteen == -1) indexFourteen = index+14 }
+    lineList[0].toCharArray().asIterable().windowed(14, 1, false).map { it.distinct() }.forEachIndexed { index, chars -> if (chars.size >= 14 && indexFourteen == -1) indexFourteen = index+14 }
     println("part2: $indexFourteen")
 }
